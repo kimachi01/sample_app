b7a3d67207f2031e50199acc6a196d2a76d09a8d
kimachi01 <kimachi@kouboum.co.jp>
2015-01-27 11:35:18 +0900

    modified config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_sample_app_session'
+#Rails.application.config.session_store :cookie_store, key: '_sample_app_session'
+SampleApp::Application.config.session_store :cookie_store, key: '_sample_app_session'
