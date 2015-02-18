2a27086cad5b8ca53d667b519b625f136f4194e5
kimachi01 <kimachi@kouboum.co.jp>
2015-02-18 10:48:00 +0900

    Moddify

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,5 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-#Rails.application.initialize!
-SampleApp::Application.initialize!
+Rails.application.initialize!
+#SampleApp::Application.initialize!
